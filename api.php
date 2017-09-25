2bf4f0fea8bb445e249b9a9ac96f8cdd9800b87e
Yogi Darusmawan <mochyogi>
2017-09-25 14:47:00 +0700

    Try Flight

---
+++ api.php
@@ -0,0 +1,37 @@
+<?php
+ 
+function login() {
+        // validasi
+    $missingParameters = array();
+    if (!isset($_POST['username'])) {
+        array_push($missingParameters, 'username');
+    }
+    if (!isset($_POST['password'])) {
+        array_push($missingParameters, 'password');
+    }
+    reportMissingParameters($missingParameters);
+        // logic
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+            // response
+    Flight::json(array(
+        "code"=>0,
+        "message"=>"Succeed",
+        "data"=>array(
+            "username"=>$username,
+            "realname"=>"Amri Shodiq"
+        )
+    ));
+}
+ 
+function reportMissingParameters($missingParameters) {
+    $parameters = implode(", ", $missingParameters);
+    Flight::json(array(
+        "code"=>1,
+        "message"=>"Missing parameter: ".$parameters.".",
+        "data"=>null
+    ));
+    exit();
+}
+ 
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+ 
+require 'flight/Flight.php';
+require 'api.php';
+ 
+Flight::route('POST /login', 'login');
+ 
+Flight::start();
+ 
+?>
