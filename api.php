1e2b8f6f06d06afede8412d2bd5e35bf4cae6b14
Yogi Darusmawan <mochyogi>
2017-09-26 13:57:46 +0700

    [API] masih bingung dengan index.php

--- api.php
+++ api.php
@@ -2,36 +2,40 @@
  
 function login() {
         // validasi
-    $missingParameters = array();
-    if (!isset($_POST['username'])) {
-        array_push($missingParameters, 'username');
-    }
-    if (!isset($_POST['password'])) {
-        array_push($missingParameters, 'password');
-    }
-    reportMissingParameters($missingParameters);
-        // logic
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-            // response
-    Flight::json(array(
-        "code"=>0,
-        "message"=>"Succeed",
-        "data"=>array(
-            "username"=>$username,
-            "realname"=>"Amri Shodiq"
-        )
-    ));
+    // $missingParameters = array();
+    // if (!isset($_POST['username'])) {
+    //     array_push($missingParameters, 'username');
+    // }
+    // if (!isset($_POST['password'])) {
+    //     array_push($missingParameters, 'password');
+    // }
+    // reportMissingParameters($missingParameters);
+    //     // logic
+    // $username = $_POST['username'];
+    // $password = $_POST['password'];
+    //         // response
+    // Flight::json(array(
+    //     "code"=>0,
+    //     "message"=>"Succeed",
+    //     "data"=>array(
+    //         "username"=>$username,
+    //         "realname"=>"Amri Shodiq"
+    //     )
+    // ));
+    // die(var_dump($missingParameters));
+    Flight::route('/', function(){
+  echo 'hello world!';
+});
 }
  
-function reportMissingParameters($missingParameters) {
-    $parameters = implode(", ", $missingParameters);
-    Flight::json(array(
-        "code"=>1,
-        "message"=>"Missing parameter: ".$parameters.".",
-        "data"=>null
-    ));
-    exit();
-}
+// function reportMissingParameters($missingParameters) {
+//     $parameters = implode(", ", $missingParameters);
+//     Flight::json(array(
+//         "code"=>1,
+//         "message"=>"Missing parameter: ".$parameters.".",
+//         "data"=>null
+//     ));
+//     exit();
+// }
  
 ?>
--- index.php
+++ index.php
@@ -4,7 +4,14 @@ require 'flight/Flight.php';
 require 'api.php';
  
 Flight::route('POST /login', 'login');
- 
+// Flight::map('notFound', function(){
+//     // Display custom 404 page
+//     include 'errors/404.html';
+// });
+// Flight::route('/', function(){
+//   echo 'hello world!';
+// });
+
 Flight::start();
  
 ?>
